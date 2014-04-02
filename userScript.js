89ad98f0f002581e27c58e1db8f1c364c7aa321b
Dmitry Broznich <xmbroz@gmail.com>
2014-04-02 09:31:52 +0400

    added possible to switch branch for debug

--- userScript.js
+++ userScript.js
@@ -1,15 +1,16 @@
 // ==UserScript==
 // @name       fixTimer
-// @version    0.60
+// @version    0.62
 // @description Script for fix bugs with time-tracker plugin and add some features in redmine
-// @match      TYPE_REDMINE_SITE_URL (Example: http://rm.innomdc.com/*)
+// @match      http://rm.innomdc.com/*
 // @copyright  2014+, Broznich Dmitry
 // ==/UserScript==
+var debugBranch = localStorage.getItem('ht-debug-branch');
 
-window.tsRepoBaseUrl = location.protocol + '//rawgithub.com/broznich/time-tracker-script/dev/';
+tsRepoBaseUrl = location.protocol + '//rawgithub.com/broznich/time-tracker-script/'+(debugBranch || 'master')+'/';
 document.head.appendChild((function(){ 
     var el = document.createElement('script');
     el.type = 'text/javascript';
-    el.src = window.tsRepoBaseUrl + 'loader.js';
+    el.src = tsRepoBaseUrl + 'loader.js';
     return el;
 })());

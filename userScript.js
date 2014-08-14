73aa854f78029a74fc810a5c20f9ed13a7b5eab9
broznich <xmbroz@gmail.com>
2014-08-14 14:43:18 +0400

    fix for firefox browser.

--- userScript.js
+++ userScript.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name       fixTimer
 // @version    0.62
+// @grant       none
 // @description Script for fix bugs with time-tracker plugin and add some features in redmine
 // @match      http://rm.innomdc.com/*
 // @copyright  2014+, Broznich Dmitry
 // ==/UserScript==
 var debugBranch = localStorage.getItem('ht-debug-branch');
 
-tsRepoBaseUrl = location.protocol + '//rawgithub.com/broznich/time-tracker-script/'+(debugBranch || 'master')+'/';
+window.tsRepoBaseUrl = location.protocol + '//rawgithub.com/broznich/time-tracker-script/'+(debugBranch || 'master')+'/';
 document.head.appendChild((function(){ 
     var el = document.createElement('script');
     el.type = 'text/javascript';

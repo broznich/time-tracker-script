53d465f7146c324127e4c6090bef259f25d503aa
broznich <xmbroz@gmail.com>
2015-02-10 14:34:10 +0300

    Prod script was migrated to S3 and host use force https

--- userScript.js
+++ userScript.js
@@ -3,12 +3,12 @@
 // @version    0.62
 // @grant       none
 // @description Script for fix bugs with time-tracker plugin and add some features in redmine
-// @match      http://rm.innomdc.com/*
+// @match      https://rm.innomdc.com/*
 // @copyright  2014+, Broznich Dmitry
 // ==/UserScript==
 var debugBranch = localStorage.getItem('ht-debug-branch');
 
-window.tsRepoBaseUrl = location.protocol + '//rawgithub.com/broznich/time-tracker-script/'+(debugBranch || 'master')+'/';
+window.tsRepoBaseUrl = location.protocol + (debugBranch ? '//rawgit.com/broznich/time-tracker-script/'+(debugBranch || 'master')+'/' : '//s3-eu-west-1.amazonaws.com/gui.bookmarklets/time-tracker/');
 document.head.appendChild((function(){ 
     var el = document.createElement('script');
     el.type = 'text/javascript';
